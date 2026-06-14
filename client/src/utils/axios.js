eb20a1b917b0d40192c11f3b6e55ac36179938af
Komal4567 <komalsinghsep04@gmail.com>
2026-06-14 17:54:24 +0530

    fix: update API URL to production backend

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://eventora-backend-2ghh.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
